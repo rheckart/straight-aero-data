81d0601361a1ae67289e28dd0d852750c98b7f8b
Rob Heckart <rob.heckart@gmail.com>
2020-07-07 23:09:12 -0400

    Lots of additional work convering to mapper. Success doing the first imports.

--- StraightAero.AirportData/StraightAero.AirportData.Main/LandingFacility.cs
+++ StraightAero.AirportData/StraightAero.AirportData.Main/LandingFacility.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 
 namespace StraightAero.AirportData.Main
 {
-	public class LandingFacility
+	public class LandingFacilityData
 	{
+		public string RecordTypeIndicator { get; set; }
 		public string LandingFacilitySiteNumber { get; set; }
 		public string LandingFacilityType { get; set; }
 		public string LocationIdentifier { get; set; }
@@ -84,19 +85,19 @@ namespace StraightAero.AirportData.Main
 		public string LensColorOfOperableBeaconLocatedOnTheAirport { get; set; }
 		public string LandingFeeChargedToNoncommercialUsersOf { get; set; }
 		public string AYInThisFieldIndicatesThatTheLanding { get; set; }
-		public int SingleEngineGeneralAviationAircraft { get; set; }
-		public int MultiEngineGeneralAviationAircraft { get; set; }
-		public int JetEngineGeneralAviationAircraft { get; set; }
-		public int GeneralAviationHelicopter { get; set; }
-		public int OperationalGliders { get; set; }
-		public int OperationalMilitaryAircraftIncludingHelicopters { get; set; }
-		public int UltralightAircraft { get; set; }
-		public int CommercialServices { get; set; }
-		public int CommuterServices { get; set; }
-		public int AirTaxi { get; set; }
-		public int GeneralAviationLocalOperations { get; set; }
-		public int GeneralAviationItinerantOperations { get; set; }
-		public int MilitaryAircraftOperations { get; set; }
+		public int? SingleEngineGeneralAviationAircraft { get; set; }
+		public int? MultiEngineGeneralAviationAircraft { get; set; }
+		public int? JetEngineGeneralAviationAircraft { get; set; }
+		public int? GeneralAviationHelicopter { get; set; }
+		public int? OperationalGliders { get; set; }
+		public int? OperationalMilitaryAircraftIncludingHelicopters { get; set; }
+		public int? UltralightAircraft { get; set; }
+		public int? CommercialServices { get; set; }
+		public int? CommuterServices { get; set; }
+		public int? AirTaxi { get; set; }
+		public int? GeneralAviationLocalOperations { get; set; }
+		public int? GeneralAviationItinerantOperations { get; set; }
+		public int? MilitaryAircraftOperations { get; set; }
 		public DateTime TwelveMonthEndingDateOnWhichAnnualOperationsData { get; set; }
 		public string AirportPositionSource { get; set; }
 		public DateTime AirportPositionSourceDate { get; set; }
@@ -109,11 +110,8 @@ namespace StraightAero.AirportData.Main
 		public string IcaoIdentifier { get; set; }
 		public string MinimumOperationalNetworkmon { get; set; }
 		public string AirportRecordFillerBlank { get; set; }
-		
-		public List<FacilityAttendanceScheduleData> FacilityAttendanceScheduleData { get; set; }
-		public List<FacilityRunwayData> FacilityRunwayData { get; set; }
-		public List<RunwayArrestingSystemData> RunwayArrestingSystemData { get; set; }
-		public List<FacilityRemarkData> FacilityRemarkData { get; set; }
+
+		public List<FacilityRunwayData> Runways { get; set; }
 	}
 
 	public class FacilityAttendanceScheduleData
--- StraightAero.AirportData/StraightAero.Data.Importer/Program.cs
+++ StraightAero.AirportData/StraightAero.Data.Importer/Program.cs
@@ -1,54 +1,68 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
+using System.Text;
 using FlatFiles;
 using FlatFiles.TypeMapping;
 using StraightAero.AirportData.Main;
-using StraightAero.AirportData.Main.Schemas;
+using StraightAero.AirportData.Main.Mappers;
 
 namespace StraightAero.Data.Importer
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Starting");
 
-            var selector = new FixedLengthSchemaSelector();
-            selector.When(s => s.StartsWith("APT")).Use(LandingFacilitySchema.GetSchema());
+            var selector = new FixedLengthTypeMapperSelector();
+            selector.When(s => s.StartsWith("APT")).Use(LandingFacilityMapper.GetLandingFacilityDataMapper());
             selector.When(s => s.StartsWith("ATT"))
-                .Use(LandingFacilitySchema.GetFacilityAttendanceScheduleSchema());
-            selector.When(s => s.StartsWith("RWY")).Use(LandingFacilitySchema.GetRunwaySchema());
+                .Use(LandingFacilityMapper.GetFacilityAttendanceScheduleDataMapper());
+            selector.When(s => s.StartsWith("RWY")).Use(LandingFacilityMapper.GetFacilityRunwayDataMapper());
             selector.When(s => s.StartsWith("ARS"))
-                .Use(LandingFacilitySchema.GetRunwayArrestingSystemSchema());
-            selector.When(s => s.StartsWith("RMK")).Use(LandingFacilitySchema.GetRemarkSchema());
+                .Use(LandingFacilityMapper.GetRunwayArrestingSystemDataMapper());
+            selector.When(s => s.StartsWith("RMK")).Use(LandingFacilityMapper.GetFacilityRemarkDataMapper());
 
-            using (var reader = new StreamReader(File.OpenRead(@"C:\Users\rheck\Downloads\28DaySubscription_Effective_2020-01-30\APT.txt")))
+            var mapit = new StringBuilder();
+
+            using var reader = new StreamReader(File.OpenRead(@"C:\Users\rheck\Downloads\28DaySubscription_Effective_2020-01-30\APT.txt"));
+            
+            var flr = selector.GetReader(reader);
+
+            LandingFacilityData airport;
+
+            while (flr.Read())
             {
-                var flr = new FixedLengthReader(reader, selector);
-                
-                while (flr.Read())
-                {
-                    var values = flr.GetValues();
-                    var airport = new LandingFacility();
+                var test = flr.Current;
 
-                    if (values != null)
-                    {
-                        if ((string)values[0] == "APT")
-                        {
-                            ProcessAirport(airport, values);                            
-                            
-                            Console.WriteLine($"New Airport: {(string)values[3]}");
-                        }
-                    }
+                if (test is LandingFacilityData)
+                {
+                    airport = (LandingFacilityData)test;
                 }
+                //var values = flr.GetValues();
+
+                //if (values != null)
+                //{
+                //    if ((string)values[0] == "APT")
+                //    {
+                //        //var lfMapper = FixedLengthTypeMapper.Define<LandingFacility>();
+                //        //var schema = mapper.GetSchema();
+                //        //lfMapper.
+
+                //        //ProcessAirport(airport, values);                            
+
+                //        //Console.WriteLine($"New Airport: {(string)values[3]}");
+                //    }
+                //}
             }
         }
 
-        private static void ProcessAirport(LandingFacility airport, object[] values)
-        {
-            airport.LandingFacilitySiteNumber = (string)values[1];
-            airport.LandingFacilityType = (string)values[2];
-            airport.LocationIdentifier = (string)values[3];
-        }
+        //private static void ProcessAirport(LandingFacility airport, object[] values)
+        //{
+        //    airport.LandingFacilitySiteNumber = (string)values[1];
+        //    airport.LandingFacilityType = (string)values[2];
+        //    airport.LocationIdentifier = (string)values[3];
+        //}
     }
 }
